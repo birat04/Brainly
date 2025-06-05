399d4a9bed36dd7ee13944a8951658f1a61b921c
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-06-05 21:41:37 +0530

    change in variable

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,15 @@
-export function random(len: number) {
-    let options = "qwertyuioasdfghjklzxcvbnm12345678";
-    let length = options.length;
+export function random(len: number): string {
+    if (len <= 0 || !Number.isInteger(len)) {
+        throw new Error("Length must be a positive integer.");
+    }
+
+    const options = "qwertyuioasdfghjklzxcvbnm12345678";
+    const length = options.length;
 
     let ans = "";
 
     for (let i = 0; i < len; i++) {
-        ans += options[Math.floor((Math.random() * length))]
+        ans += options[Math.floor(Math.random() * length)];
     }
 
     return ans;
